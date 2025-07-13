8606a8eb371601d8aaf08f28a21670015d615238
Zuzanna Osuch <zuzanna.osuch@wp.pl>
2025-07-13 19:11:45 +0200

    dark

--- js/script.js
+++ js/script.js
@@ -72,3 +72,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.getElementById('darkSwitch').addEventListener('change', function() {
+    if (this.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.removeAttribute('data-theme');
+    }
+});
